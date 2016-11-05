1abf537873bf16a16bb2edfd34edc219b76045a9
Camila Alvarez <cam.alvarez.i@gmail.com>
2016-11-04 23:52:03 -0300

    Basic add label function

--- clothingClassifier/clothingClassifier/ViewController.swift
+++ clothingClassifier/clothingClassifier/ViewController.swift
@@ -18,6 +18,7 @@ class CropViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
     @IBOutlet var photoView:AdaptableRectangle!
     @IBOutlet var categoryPicker:UIPickerView!
     private let categories:[category] = CropViewController.loadCategoriesIntoArray()
+    private var currentImage:Image!
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -39,9 +40,10 @@ class CropViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.layoutIfNeeded()
+        currentImage = Image(imageId: 1, imageUrl: "https://s-media-cache-ak0.pinimg.com/564x/5c/a5/28/5ca5280505933a20ba48869ca9722ad3.jpg")
         let imageData = try? Data(contentsOf: URL(string: "https://s-media-cache-ak0.pinimg.com/564x/5c/a5/28/5ca5280505933a20ba48869ca9722ad3.jpg")!)
         let image = UIImage(data: imageData!)
-        photoView.image = image
+        photoView.image = currentImage.getImage()
         
     }
     
@@ -63,15 +65,53 @@ class CropViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
     }
 
     @IBAction func addLabel(sender:UIButton)->Void{
-        let rect = photoView.getCroppedRect()
-        print("view: \(photoView.frame.size)")
-        print("image: \(photoView.image?.size)")
-        
-        print("view point: \(rect.origin)")
-        print("image point : \(photoView.convertPoint(fromView: rect.origin))")
-        
+        let boundingBox = photoView.convertRect(fromView: photoView.getCroppedRect())
         let selectedRow = categoryPicker.selectedRow(inComponent: 0)
-        print("class: \(categories[selectedRow].categoryDescription)")
+        do{
+            let categoryIdQuery = try Categories()?.get(columns: ["cat_id"])
+                .whereCond(conditions: ["cat_code":categories[selectedRow].categoryCode])
+            categoryIdQuery?.exec(){results in
+                //There should only be one result
+                guard results.count == 1 else{
+                    fatalError("Invalid database result")
+                }
+                let catId = results[0]["cat_id"] as! String
+                let insertValues:[String:Any] = ["lbl_image_id":self.currentImage.getImageId(),
+                                                 "lbl_x": boundingBox.minX,
+                                                 "lbl_y": boundingBox.minY,
+                                                 "lbl_height": boundingBox.height,
+                                                 "lbl_width": boundingBox.width,
+                                                 "lbl_category": catId]
+                do{
+                    let insertLabelQuery = try Labels()?.insert(values: insertValues)
+                    insertLabelQuery?.execUpdate(){success in
+                        if success{
+                        //Crear e insertar rectangulo
+                            //RECORDAR QUE LA ACTUALIZACION DE LA INTERFAZ SE DEBE HACER EN EL MAIN THREAD
+                            //=>ACTUALIZACION ES AGREGAR LAS SUBVISTAS QUE SERAN LOS 4 COSTADOS Y EL RECTANGULO CON 
+                            //EL LABEL
+                        }
+                    }
+                }
+                catch queryErrors.badQuery{
+                    print("INVALID INSERT")
+                }
+                catch{
+                    print("UNKNOWN ERROR")
+                }
+                
+            }
+
+        }
+        catch queryErrors.badQuery{
+            print("INVALID GET QUERY")
+        }
+        catch queryErrors.invalidCondition{
+            print("INVALID WHERE CONDITION")
+        }
+        catch{
+            print("UNKNOWN ERROR")
+        }
     }
     
 
